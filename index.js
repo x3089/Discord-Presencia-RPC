866d2f5a9496fbc6ed9d084d334c9c4037e45c69
Astra <astrapch4@gmail.com>
2025-04-07 23:11:09 +0200

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Proyecto Archivo 1
 // Este código pertenecía a otro usuario, solamente lo hice funcionar y minimalice el código para una mejor función,
 // pronto lanzaré mi propia versión premium.
 
